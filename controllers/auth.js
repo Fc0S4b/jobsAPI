a316a5fc5787ee4990e791da8097e68ce7781e23
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-23 16:37:31 -0400

    error checking- controller example

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,12 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-
+const { BadRequestError } = require('../errors');
 const register = async (req, res) => {
+  // const { name, email, password } = req.body;
+  // if (!name || !email || !password) {
+  //   throw new BadRequestError('Please provide name, email and password');
+  // }
+
   const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
