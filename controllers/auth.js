e169f91df2eca8a65aab407c171032a62195e917
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-23 23:20:49 -0400

    generate token - controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,14 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 const login = async (req, res) => {
   res.send('login user');
