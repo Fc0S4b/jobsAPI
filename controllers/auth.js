1e3ec6c75c14608de8574dfd3c61157f2d3e1663
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-23 22:53:00 -0400

    Mongoose middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,15 +4,7 @@ const { BadRequestError } = require('../errors');
 const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-  // nunca guardar passwords como string en base de datos, debes hashearlas, usa la librería bcryptjs
-  const { name, email, password } = req.body;
-
-  const salt = await bcrypt.genSalt(10); //random bytes que se agregan a la contraseña
-  const hashedPassword = await bcrypt.hash(password, salt); //hasheamos la passowrd
-
-  const tempUser = { name, email, password: hashedPassword };
-
-  const user = await User.create({ ...tempUser });
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -22,6 +22,11 @@ const UserSchema = new mongoose.Schema({
     minlength: 6,
     // maxlength: 12, hash es mayor que 12
   },
+});
+
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model('User', UserSchema);
