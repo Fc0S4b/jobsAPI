ce2e80ebbc5f376d8fc82c16d9b83e2697bd16e7
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-23 23:29:34 -0400

    generate token - instance method

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -28,5 +29,14 @@ UserSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// UserSchema.methods.getName = function () {
+//   return this.name;
+// };
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema);
