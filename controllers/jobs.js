552c4a8002bdccc81f79b6901ac17a8c21d79556
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-25 22:53:28 -0400

    Get All Jobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,7 +3,8 @@ const Job = require('../models/Job');
 const { BadRequestError, NotFoundError } = require('../errors');
 
 const getAllJobs = async (req, res) => {
-  res.send('get all jobs');
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 const getJob = async (req, res) => {
   res.send('get job');
