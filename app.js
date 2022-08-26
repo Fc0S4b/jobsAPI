0d7b2066ce829522d8fd4427d253f736f24cc3c3
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-26 16:59:28 -0400

    security setup

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security package
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -13,8 +20,17 @@ const jobsRouter = require('./routes/jobs');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-
+app.set('trust proxy', 1); //Enable if you're behind a reverse proxy (Heroky, Bluemix, AWS ELB, Ngin)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, //limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
