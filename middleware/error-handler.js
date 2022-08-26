260b0528e60177bf72b5c613622f24113a8675f6
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-26 02:16:14 -0400

    Custom Error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,9 +7,9 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'Something went wrong try again later',
   };
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
