d21604e97b59fee3d3aa483eb359742992915ce5
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-25 21:33:20 -0400

    Auth Middleware - Testing

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -5,7 +5,7 @@ const getJob = async (req, res) => {
   res.send('get job');
 };
 const createJob = async (req, res) => {
-  res.send('create job');
+  res.json(req.user);
 };
 const updateJob = async (req, res) => {
   res.send('update job');
